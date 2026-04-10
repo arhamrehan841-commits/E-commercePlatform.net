af76c385ff95925623fa3895e05ae48b65802ab4
Arham Rehan <arhamrehan841@gmail.com>
2026-04-10 16:16:21 +0500

    refactor(host): make migrations async and wire up catalog seeder

--- src/Host/Extensions/MigrationExtensions.cs
+++ src/Host/Extensions/MigrationExtensions.cs
@@ -6,15 +6,19 @@ namespace Host.Extensions;
 
 public static class MigrationExtensions
 {
-    public static void ApplyMigrations(this IApplicationBuilder app)
+    // Changed to async Task to support database seeding
+    public static async Task ApplyMigrationsAsync(this IApplicationBuilder app)
     {
         using IServiceScope scope = app.ApplicationServices.CreateScope();
 
         var catalogContext = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
         var ordersContext = scope.ServiceProvider.GetRequiredService<OrdersDbContext>();
 
-        // Applies pending .cs migrations to the physical database
-        catalogContext.Database.Migrate();
-        ordersContext.Database.Migrate();
+        // 1. Build the tables
+        await catalogContext.Database.MigrateAsync();
+        await ordersContext.Database.MigrateAsync();
+
+        // 2. Populate the tables
+        await CatalogDataSeeder.SeedAsync(catalogContext);
     }
 }
