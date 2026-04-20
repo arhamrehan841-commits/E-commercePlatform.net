52df10ff9600bdb8171b5904e1c9f3894bc8e7e3
Arham Rehan <arhamrehan841@gmail.com>
2026-04-20 23:34:59 +0500

    refactor(CatalogModuleExtension.cs): added dependency registration -  services.AddScoped<IStockReservationContract, CatalogReservationService>();

--- src/Modules/Catalog/Infrastructure/CatalogModuleExtensions.cs
+++ src/Modules/Catalog/Infrastructure/CatalogModuleExtensions.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SharedKernel.Database;
+using SharedKernel.Contracts; // Add this for IStockReservationContract
+using Modules.Catalog.Infrastructure.Contracts;
 using Modules.Catalog.Infrastructure.Data;
 using Modules.Catalog.Application.Products.Create; // Add this
 
@@ -13,6 +15,7 @@ public static class CatalogModuleExtensions
         // 1. Database Setup
         services.AddDbContext<CatalogDbContext>(opt => opt.UseSqlServer(connectionString));
         services.AddScoped<IModuleDatabase, CatalogModuleDatabase>();
+        services.AddScoped<IStockReservationContract, CatalogReservationService>();
         
         // 2. MediatR Setup (Moved from Host!)
         services.AddMediatR(config => 
